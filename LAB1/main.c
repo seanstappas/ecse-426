6623497c000212c510c2959480689673d4e9769a
Eric Vuong <ericvuong22222@gmail.com>
2018-01-30 16:31:06 -0500

    Added FIR code + integration tst

--- LAB1/main.c
+++ LAB1/main.c
@@ -57,17 +57,22 @@ int CMSIS_math(float* input_array, float* output_array, int array_length) {
 	return 0;
 }
 
+
+
 // FIR function
-int FIR_C(int* input_array, float* output_array) {
+int FIR_C(int* input_array, float* output_array, int array_length) {
 	//coefficient of the filter
 	float coeff[5] = {0.1,0.15,0.5,0.15,0.1};
 	int order = 5;
 	//loop through array
-	for(int i = order-1;i<(sizeof(input_array)/sizeof(input_array[0]));i++){
-		int sum=0;
+	for(int i = 0;i<array_length;i++){
+		float sum = 0.0;
 			//compute sum
-			for(int j = 0; j < order; j++){
-				sum += input_array[i]*coeff[j];
+			for(int j = order-1; j >=0; j--){
+					int sumIndex = i-j;
+					if(sumIndex>=0){
+						sum += input_array[sumIndex]*coeff[j];
+					}
 			}
 			output_array[i] = sum;
 	}
@@ -75,7 +80,7 @@ int FIR_C(int* input_array, float* output_array) {
 	return 0;
 }
 
-int test_array(float* input_array, int array_length) {
+int test_math_functions(float* input_array, int array_length) {
 	for (int i = 0; i < array_length; i++) {
 		printf("Element %i of input: %f\n", i, input_array[i]);
 	}
@@ -118,22 +123,43 @@ int test_array(float* input_array, int array_length) {
 	return 0;
 }
 
+int test_filter() {
+	int input_array2[] = {-3,-4,3};
+	int array_length2 = sizeof(input_array2)/sizeof(int);
+	float output_array2[array_length2];
+	FIR_C(input_array2, output_array2, array_length2);
+	
+	for (int i = 0; i < array_length2; i++) {
+		printf("index %i: %f\n", i, output_array2[i]);
+	}
+	
+	return 0;
+}
+
 int main() {
 	//float input_array1[] = {0.1, 5.2, 3.1, 8.0};
 	//int array_length = sizeof(input_array1) / sizeof(float);
-	//test_array(input_array1, array_length);
+	//test_math_functions(input_array1, array_length);
 	
 	//float input_array2[] = {-51040, 5002.32, 3001.21, 8414, 40402.3};
 	//int array_length = sizeof(input_array2) / sizeof(float);
-	//test_array(input_array2, array_length);
+	//test_math_functions(input_array2, array_length);
 	
 	//float input_array3[] = {0.3311, 14124.322, 300.21, 800.323};
 	//int array_length = sizeof(input_array3) / sizeof(float);
-	//test_array(input_array3, array_length);
+	//test_math_functions(input_array3, array_length);
 	
-	float input_array4[] = {0.131231, 0.51232, 0.1231321, 0.1231238};
-	int array_length = sizeof(input_array4) / sizeof(float);
-	test_array(input_array4, array_length);
+	//test_filter();
+	
+	//float input_array4[] = {0.131231, 0.51232, 0.1231321, 0.1231238};
+	//int array_length = sizeof(input_array4) / sizeof(float);
+	//test_math_functions(input_array4, array_length);
+	
+	int input_array[] = {-3,-4,3,4,20,10};
+	int array_length = sizeof(input_array) / sizeof(float);
+	float output_array[array_length];
+	FIR_C(input_array, output_array, array_length);
+	test_math_functions(output_array, array_length);
 	
 	return 0;
 }

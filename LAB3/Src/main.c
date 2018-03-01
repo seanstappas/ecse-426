044d11d427d05d8302d96ec82450cb86939a574b
Eric Vuong <ericvuong22222@gmail.com>
2018-03-01 16:07:57 -0500

    updated tim3 period for pwm

--- LAB3/Src/main.c
+++ LAB3/Src/main.c
@@ -747,7 +747,7 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 83;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 99;
+  htim3.Init.Period = 100;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
   {

0dc3dbf30a00fda9c5ab1a10a4e605eb821cf2dd
Sean Stappas <seanstappas@gmail.com>
2018-02-28 23:12:20 -0500

    Timer with ADC DMA working

--- LAB3/Src/main.c
+++ LAB3/Src/main.c
@@ -731,9 +731,9 @@ static void MX_TIM2_Init(void)
   TIM_MasterConfigTypeDef sMasterConfig;
 
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 839;
+  htim2.Init.Prescaler = 84;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 0;
+  htim2.Init.Period = 10;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
   {
